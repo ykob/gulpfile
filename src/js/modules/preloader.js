89e2f7cdf539f501ff9c0979dec35369a66b32d5
yoichi kobayashi <info@tplh.net>
2016-05-27 17:25:21 +0900

    [modify] cleanup prealoder class.

--- src/js/modules/preloader.js
+++ src/js/modules/preloader.js
@@ -1,76 +1,51 @@
 export default class Preloader {
-  constructor(json, callback) {
-    this.$sequencer = $('.c-preloader__sequencer');
-    this.file_list = json;
+  constructor() {
     this.data = null;
+    this.callbackLoadedData = null;
+    this.callbackLoadedDataAll = null;
     this.count_loaded = 0;
-    this.callback = callback;
     this.complete = false;
-    this.loadJSON();
   }
-  loadJSON() {
-    $.ajax({
-      url: this.file_list,
-      type: 'GET',
-      dataType: 'json'
-    })
-    .done((data) => {
-      this.data = data;
-      this.preload();
-    })
-    .fail(() => {
-      alert("error load filelist json.");
-    });
-  }
-  loadImage(data) {
-    const image = new Image();
-    image.onload = () => {
-      this.loadedData(data);
-    };
-    image.src = data;
-  }
-  preload() {
+  start(data, callbackLoadedData, callbackLoadedDataAll) {
+    if (!data.isArray) return;
+    this.data = data;
+    this.callbackLoadedData = callbackLoadedData;
+    this.callbackLoadedDataAll = callbackLoadedDataAll;
     const reg = /(.*)(?:\.([^.]+$))/;
     for (var i = 0; i < this.data.length; i++) {
       const data = this.data[i];
       switch (data.match(reg)[2]) {
         case 'png':
-          this.loadImage(data);
-          break;
         case 'jpg':
-          this.loadImage(data);
-          break;
         case 'gif':
-          this.loadImage(data);
+          const image = new Image();
+          image.onload = () => {
+            this.loadedData();
+          };
+          image.src = data;
           break;
         case 'mp4':
           const video = document.createElement('video');
+          video.addEventListener('loadeddata', () => {
+            this.loadedData();
+          });
           video.src = data;
           video.load();
-          this.loadedData(data);
-          // video.onloadeddata = () => {
-          //   this.loadedData(data);
-          // };
           break;
         default:
+          this.loadedData();
       }
     }
   }
-  loadedData(data) {
+  loadedData() {
     this.count_loaded++;
-    this.$sequencer.css({
-      width: `${this.count_loaded / this.data.length * 100}%`
-    });
+    if (this.callbackLoadedData) this.callbackLoadedData();
     if (this.count_loaded >= this.data.length) {
       this.loadedDataAll();
     }
   }
   loadedDataAll() {
     this.complete = true;
-    $('.c-preloader').addClass('is-loaded');
-    setTimeout(() => {
-      $('.l-page').addClass('is-loaded');
-      this.callback();
-    }, 500);
+    if (this.callbackLoadedDataAll) this.callbackLoadedDataAll();
   }
 }
