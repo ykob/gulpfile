c6044b13721b07300fead3c1e7ced63b4fba45fd
yoichi kobayashi <info@tplh.net>
2016-08-18 20:17:29 +0900

    [add] function initCommon.

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
+import initCommon from './init/common.js'
 import initIndex from './init/index.js'
 
 const { pathname } = window.location;
 
 const init = () => {
+  initCommon();
   switch (pathname.replace('index.html', '')) {
     case '/':
       initIndex();
