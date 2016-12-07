8c61b3479716b5485aa5cd713587817ef5b3b9c3
yoichi kobayashi <info@tplh.net>
2016-12-07 16:36:45 +0900

    [modify] FixBody class hold scroll top value.

--- src/js/modules/FixBody.js
+++ src/js/modules/FixBody.js
@@ -1,12 +1,14 @@
 export default class FixBody {
   constructor() {
-    this.$window = $(window);
-    this.$body = $('body');
+    this.$window = jQuery(window);
+    this.$body = jQuery('body');
+    this.top = 0;
   }
   set() {
+    this.top = this.$window.scrollTop()
     this.$body.css({
       position: 'fixed',
-      marginTop: this.$window.scrollTop() * -1
+      marginTop: this.top * -1
     });
   }
   cancel() {
@@ -14,5 +16,6 @@ export default class FixBody {
       position: 'static',
       marginTop: 0
     });
+    this.$window.scrollTop(this.top)
   }
 }
