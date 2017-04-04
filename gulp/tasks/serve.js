eb90dd75d608c903854fb826d968b54eaac9a7de
yoichi kobayashi <info@tplh.net>
2017-04-04 18:40:46 +0900

    [modify] clean up the serve task.

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -6,44 +6,35 @@ const fs = require('fs');
 const url = require("url");
 const pug = require('pug');
 
-const $ = require('../plugins');
 const conf = require('../conf').serve;
 const confPug = require('../conf').pug;
 
 const getPugTemplatePath = (baseDir, req)=>{
   const requestPath = url.parse(req.url).pathname;
-  const suffix = path.parse(requestPath).ext ? "": "index.html"
+  const suffix = path.parse(requestPath).ext ? '' : 'index.html';
   return path.join(baseDir, "src/html", requestPath, suffix);
 }
 
 const pugMiddleWare = (req, res, next) => {
-  const basedir = process.cwd();
-  const requestPath = getPugTemplatePath(basedir,req)
-
-  if (path.parse(requestPath).ext !== ".html") {
+  const requestPath = getPugTemplatePath(process.cwd(), req);
+  if (path.parse(requestPath).ext !== '.html') {
     return next();
   }
   const pugPath = requestPath.replace('.html', '.pug');
-  const data = JSON.parse(fs.readFileSync(confPug.json));
-
-  try {
-    console.log("[BS] try to file "+ pugPath)
-    const content = pug.renderFile(pugPath, {
-      data: data,
-      pretty: true,
-    });
-    res.end(new Buffer(content));
-  } catch (e) {
-    console.log(e);
-    return next();
-  }
+  console.log("[BS] rendering pug : "+ pugPath);
+  const content = pug.renderFile(pugPath, {
+    data: JSON.parse(fs.readFileSync(confPug.json)),
+    pretty: true,
+  });
+  res.end(new Buffer(content));
 }
 
 gulp.task("serve",()=> {
-  conf.dest.server.middleware = [pugMiddleWare];
   if (yargs.build == true) {
+    conf.build.server.middleware = [pugMiddleWare];
     browserSync(conf.build);
   } else {
+    conf.dest.server.middleware = [pugMiddleWare];
     browserSync(conf.dest);
   }
 });
