257b0162f77eacdf7cec8fe161d33e55b003d03a
yoichi kobayashi <info@tplh.net>
2017-08-31 18:34:44 +0900

    tweaked.

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-const pageId = document.getElementsByClassName('l-page')[0].getAttribute('data-page-id');
+const pageId = document.querySelector('.l-page').getAttribute('data-page-id');
 
 const init = () => {
   require ('./init/common.js').default();
