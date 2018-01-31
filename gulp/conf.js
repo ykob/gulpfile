f9347b47ee95e9692fa036f5b11c4540f8a91409
yoichi kobayashi <info@tplh.net>
2018-01-31 12:51:34 +0900

    replace some processes and files about spritesmith.

--- gulp/conf.js
+++ gulp/conf.js
@@ -128,7 +128,6 @@ module.exports.copy = {
   dest: {
     src: [
       `${DIR.SRC}/img/**/*.*`,
-      `!${DIR.SRC}/img/sprite/*.*`,
       `${DIR.SRC}/font/**/*.*`,
     ],
     dest: `${DIR.DEST}${DIR.PATH}`,
