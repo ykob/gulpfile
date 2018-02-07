8bc5761d039dc938bf14d44874dd2072e828d8bf
yoichi kobayashi <info@tplh.net>
2018-02-07 20:30:41 +0900

    included sub directory path into RegExp at gulp replace task.

--- gulp/tasks/replace.js
+++ gulp/tasks/replace.js
@@ -4,8 +4,10 @@ const $ = require('../plugins');
 const conf = require('../conf').replace;
 
 gulp.task('replaceHtml', () => {
+  const regJs = new RegExp(`(src="${conf.html.path}\/js\/)([a-z0-9_\.\-]*)(\.js")`);
+  const regCss = new RegExp(`(href="${conf.html.path}\/css\/)([a-z0-9_\.\-]*)(\.css")`);
   return gulp.src(conf.html.src)
-    .pipe($.replace(/(src="\/js\/)([a-z0-9_\.\-]*)(\.js")/g, '$1$2.min$3'))
-    .pipe($.replace(/(href="\/css\/)([a-z0-9_\.\-]*)(\.css")/g, '$1$2.min$3'))
+    .pipe($.replace(regJs, '$1$2.min$3'))
+    .pipe($.replace(regCss, '$1$2.min$3'))
     .pipe(gulp.dest(conf.html.dest));
 });
