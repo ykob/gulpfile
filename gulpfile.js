000c4f0748f29dd06971331daf124ae4630080ab
yoichi kobayashi <info@tplh.net>
2018-02-09 14:00:50 +0900

    clean up.

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,10 @@ const runSequence = require('run-sequence');
 const browserSync = require('browser-sync');
 const reload = browserSync.reload;
 
+const $ = require('./gulp/plugins');
 const DIR = require('./gulp/conf').DIR;
 
 requireDir('./gulp/tasks');
-const $ = require('./gulp/plugins');
 
 gulp.task('predefault', cb => {
   runSequence(
