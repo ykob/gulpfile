38337b055f22df96be5bfeb402a875f1dcd64ca0
yoichi kobayashi <info@tplh.net>
2018-03-22 20:36:04 +0900

    added javascript easing file.

---
+++ src/js/modules/common/easings.js
@@ -0,0 +1,12 @@
+module.exports = {
+  expoOut: function(t) {
+    return (t == 1.0) ? t : 1.0 - Math.pow(2.0, -10.0 * t);
+  },
+  expoInOut: function(t) {
+    return (t == 0.0 || t == 1.0)
+      ? t
+      : (t < 0.5)
+        ? + 0.5 * Math.pow(2.0, (20.0 * t) - 10.0)
+        : - 0.5 * Math.pow(2.0, 10.0 - (t * 20.0)) + 1.0;
+  },
+}
