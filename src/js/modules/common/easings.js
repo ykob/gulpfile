4b0187c9b5c9ca6b5ec70abdf3e78f29cdcebaed
yoichi kobayashi <info@tplh.net>
2018-03-23 02:20:19 +0900

    added javascript easing function named circOut and circInOut..

--- src/js/modules/common/easings.js
+++ src/js/modules/common/easings.js
@@ -9,4 +9,12 @@ module.exports = {
         ? + 0.5 * Math.pow(2.0, (20.0 * t) - 10.0)
         : - 0.5 * Math.pow(2.0, 10.0 - (t * 20.0)) + 1.0;
   },
+  circOut: function (t) {
+    return Math.sqrt((2.0 - t) * t);
+  },
+  circInOut: function (t) {
+    return (t < 0.5)
+      ? 0.5 * (1.0 - Math.sqrt(1.0 - t * t))
+      : 0.5 * (Math.sqrt(1 - (t -= 2) * t) + 1) + 1.0);
+  },
 }
