fdd6d152dd4e2a98d7561af737541b55607c01ec
yoichi kobayashi <info@tplh.net>
2018-03-29 14:05:55 +0900

    added new module viewport-units-buggyfill to fixe a bug that iOS doesn't return right value of unit "vh".

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,8 @@
+const viewportUnitsBuggyfill = require('viewport-units-buggyfill');
 const pageId = document.querySelector('.l-page').getAttribute('data-page-id');
+
+// iOSでvhがステータスバー等を含めた値になってしまう現象を viewport-units-buggyfill で補正
+viewportUnitsBuggyfill.init();
 
 const init = () => {
   require ('./init/common.js').default();
