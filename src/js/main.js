4e762414bac8ff797fdd98ae3b81255173c0bb29
yoichi kobayashi <info@tplh.net>
2018-04-03 03:15:24 +0900

    load a stylesheet ansynchronously.

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 const viewportUnitsBuggyfill = require('viewport-units-buggyfill');
-const pageId = document.querySelector('.l-page').getAttribute('data-page-id');
 
-// iOSでvhがステータスバー等を含めた値になってしまう現象を viewport-units-buggyfill で補正
-viewportUnitsBuggyfill.init();
+const pageId = document.querySelector('.l-page').getAttribute('data-page-id');
+const ua = window.navigator.userAgent;
+const link = document.querySelector('link[as=style]');
 
 const init = () => {
   require ('./init/common.js').default();
@@ -13,4 +13,12 @@ const init = () => {
     default:
   }
 }
+
+// iOSでvhがステータスバー等を含めた値になってしまう現象を viewport-units-buggyfill で補正
+viewportUnitsBuggyfill.init();
+
+// run initialize function.
 init();
+
+// preload stylesheet other than Google Chrome browser.
+if (ua.indexOf('Chrome') < 0) link.rel = 'stylesheet';
