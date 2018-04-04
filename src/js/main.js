2c61faac3a8b922c35173cd0ef26f8a337125eae
yoichi kobayashi <info@tplh.net>
2018-04-04 19:54:41 +0900

    Change the initialization order of JavaScript so that stylesheets are loaded before other processes.

--- src/js/main.js
+++ src/js/main.js
@@ -14,11 +14,13 @@ const init = () => {
   }
 }
 
-// iOSでvhがステータスバー等を含めた値になってしまう現象を viewport-units-buggyfill で補正
-viewportUnitsBuggyfill.init();
-
-// run initialize function.
-init();
-
 // preload stylesheet other than Google Chrome browser.
 if (ua.indexOf('Chrome') < 0) link.rel = 'stylesheet';
+
+setTimeout(() => {
+  // Making viewport units (vh|vw|vmin|vmax) work properly in Mobile Safari.
+  viewportUnitsBuggyfill.init();
+
+  // run initialize function.
+  init();
+}, 100);
