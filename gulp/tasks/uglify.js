0533675443a9bc46241416b25db56fd8effab693
yoichi kobayashi <info@tplh.net>
2018-06-06 13:37:37 +0900

    added a new process that replace image path in JavaScript files to gulp uglify task.

--- gulp/tasks/uglify.js
+++ gulp/tasks/uglify.js
@@ -2,19 +2,37 @@ const gulp = require('gulp');
 const pump = require('pump');
 
 const $ = require('../plugins');
+const DIR = require('../conf').DIR;
 const conf = require('../conf').uglify;
 
 gulp.task('uglify', (cb) => {
-  const dest = (process.env.NODE_ENV === 'production') ? conf.dest.cms : conf.dest.static;
-  pump(
-    [
-      gulp.src(conf.src),
-      $.uglify(conf.opts),
-      $.rename({
-        suffix: '.min'
-      }),
-      gulp.dest(dest)
-    ],
-    cb
-  );
+  const isProduction = process.env.NODE_ENV === 'production';
+  const dest = (isProduction) ? conf.dest.cms : conf.dest.static;
+  if (isProduction) {
+    const regBgImg = new RegExp(/([\'\"]).*?(\/img)(.*)/g);
+    pump(
+      [
+        gulp.src(conf.src),
+        $.uglify(conf.opts),
+        $.replace(regBgImg, '$1' + DIR.PATH + DIR.CMS + '/assets$2$3'),
+        $.rename({
+          suffix: '.min'
+        }),
+        gulp.dest(dest)
+      ],
+      cb
+    );
+  } else {
+    pump(
+      [
+        gulp.src(conf.src),
+        $.uglify(conf.opts),
+        $.rename({
+          suffix: '.min'
+        }),
+        gulp.dest(dest)
+      ],
+      cb
+    );
+  }
 });
