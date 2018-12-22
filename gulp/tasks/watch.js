3fc768b7bc2df28fa1d2022a3c3a38eaf2867cac
yoichi kobayashi <info@tplh.net>
2018-12-22 23:56:02 +0900

    removed run-sequence and updated the way to set parallel task and to set the watch.

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,39 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
+
+const $ = require('../plugins');
+const DIR = require('./gulp/conf').DIR;
+
+gulp.task('watch', () => {
+  $.watch(
+    [`./${DIR.SRC}/**/*.{scss,sass}`],
+    () => {
+      gulp.start(['sass'])
+    }
+  ).on('change', reload);
+
+  $.watch(
+    [`./${DIR.SRC}/**/*.pug`]
+  ).on('change', reload);
+
+  $.watch(
+    [
+      `./${DIR.SRC}/**/*.js`,
+    ],
+    () => {
+      gulp.start(['scripts'])
+    }
+  ).on('change', reload);
+
+  $.watch(
+    [
+      `./${DIR.SRC}/img/**/*.*`,
+      `./${DIR.SRC}/font/**/*.*`,
+      `./${DIR.SRC}/json/**/*.*`,
+    ],
+    () => {
+      gulp.start(['copyToDest'])
+    }
+  ).on('change', reload);
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,83 +1,35 @@
 const gulp = require('gulp');
 const requireDir = require('require-dir');
-const runSequence = require('run-sequence');
-const browserSync = require('browser-sync');
-const reload = browserSync.reload;
 
 const $ = require('./gulp/plugins');
-const DIR = require('./gulp/conf').DIR;
 
 requireDir('./gulp/tasks');
 
-gulp.task('predefault', cb => {
-  runSequence(
-    'cleanDest',
-    ['pug', 'sass', 'scripts', 'copyToDest'],
-    'serve',
-    cb
-  );
-});
+gulp.task('default', gulp.series(
+  'cleanDest',
+  gulp.parallel('pug', 'sass', 'scripts', 'copyToDest'),
+  'serve',
+  'watch',
+));
 
-gulp.task('default', ['predefault'], () => {
-  $.watch(
-    [`./${DIR.SRC}/**/*.{scss,sass}`],
-    () => {
-      gulp.start(['sass'])
-    }
-  ).on('change', reload);
+gulp.task('build', gulp.series(
+  'cleanDest',
+  gulp.parallel('pug', 'sass', 'copyToDest'),
+  'cleanBuild',
+  'replaceHtml',
+  'cleanCss',
+  'scripts',
+  'imagemin',
+  gulp.parallel('copyToBuild', 'copyPhpToBuild', 'copyCmsToBuild'),
+  'sitemap',
+));
 
-  $.watch(
-    [`./${DIR.SRC}/**/*.pug`]
-  ).on('change', reload);
+gulp.task('buildHtml', gulp.series(
+  'pug',
+  'replaceHtml',
+));
 
-  $.watch(
-    [
-      `./${DIR.SRC}/**/*.js`,
-    ],
-    () => {
-      gulp.start(['scripts'])
-    }
-  ).on('change', reload);
-
-  $.watch(
-    [
-      `./${DIR.SRC}/img/**/*.*`,
-      `./${DIR.SRC}/font/**/*.*`,
-      `./${DIR.SRC}/json/**/*.*`,
-    ],
-    () => {
-      gulp.start(['copyToDest'])
-    }
-  ).on('change', reload);
-});
-
-gulp.task('build', cb => {
-  runSequence(
-    'cleanDest',
-    ['pug', 'sass', 'copyToDest'],
-    'cleanBuild',
-    'replaceHtml',
-    'cleanCss',
-    'scripts',
-    'imagemin',
-    ['copyToBuild', 'copyPhpToBuild', 'copyCmsToBuild'],
-    'sitemap',
-    cb
-  );
-});
-
-gulp.task('buildHtml', cb => {
-  runSequence(
-    'pug',
-    'replaceHtml',
-    cb
-  );
-});
-
-gulp.task('buildCss', cb => {
-  runSequence(
-    'sass',
-    'cleanCss',
-    cb
-  );
-});
+gulp.task('buildCss', gulp.series(
+  'sass',
+  'cleanCss',
+));
